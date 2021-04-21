8160942b3f12bb50c5f6dddb2990a6c62f4b34d9
shashank2325 <shashank2325@gmail.com>
2021-04-22 00:06:50 +0530

    First commit

---
+++ Vehicles_Portal/js/kickstart.js
@@ -0,0 +1,179 @@
+
+
+jQuery(document).ready(function($){
+
+
+	/*---------------------------------
+		Tabs
+	-----------------------------------*/
+	// tab setup
+	$('.tab-content').addClass('clearfix').not(':first').hide();
+	$('ul.tabs').each(function(){
+		var current = $(this).find('li.current');
+		if(current.length < 1) { $(this).find('li:first').addClass('current'); }
+		current = $(this).find('li.current a').attr('href');
+		$(current).show();
+	});
+
+	// tab click
+	$(document).on('click', 'ul.tabs a[href^="#"]', function(e){
+		e.preventDefault();
+		var tabs = $(this).parents('ul.tabs').find('li');
+		var tab_next = $(this).attr('href');
+		var tab_current = tabs.filter('.current').find('a').attr('href');
+		$(tab_current).hide();
+		tabs.removeClass('current');
+		$(this).parent().addClass('current');
+		$(tab_next).show();
+		history.pushState( null, null, window.location.search + $(this).attr('href') );
+		return false;
+	});
+
+ 	// tab hashtag identification and auto-focus
+    	var wantedTag = window.location.hash;
+    	if (wantedTag != "")
+    	{
+			// This code can and does fail, hard, killing the entire app.
+			// Esp. when used with the jQuery.Address project.
+			try {
+				var allTabs = $("ul.tabs a[href^=" + wantedTag + "]").parents('ul.tabs').find('li');
+				var defaultTab = allTabs.filter('.current').find('a').attr('href');
+				$(defaultTab).hide();
+				allTabs.removeClass('current');
+				$("ul.tabs a[href^=" + wantedTag + "]").parent().addClass('current');
+				$("#" + wantedTag.replace('#','')).show();
+			} catch(e) {
+				// I have no idea what to do here, so I'm leaving this for the maintainer.
+			}
+    	}
+
+
+
+	/*---------------------------------
+		Table Sort
+	-----------------------------------*/
+	// init
+	var aAsc = [];
+	$('table.sortable').each(function(){
+		$(this).find('thead th').each(function(index){$(this).attr('rel', index);});
+		$(this).find('th,td').each(function(){$(this).attr('value', $(this).text());});
+	});
+
+	// table click
+	$(document).on('click', 'table.sortable thead th', function(e){
+		// update arrow icon
+		$(this).parents('table.sortable').find('span.arrow').remove();
+		$(this).append('<span class="arrow"></span>');
+
+		// sort direction
+		var nr = $(this).attr('rel');
+		aAsc[nr] = aAsc[nr]=='asc'?'desc':'asc';
+		if(aAsc[nr] == 'desc'){ $(this).find('span.arrow').addClass('up'); }
+
+		// sort rows
+		var rows = $(this).parents('table.sortable').find('tbody tr');
+		rows.tsort('td:eq('+nr+')',{order:aAsc[nr],attr:'value'});
+
+		// fix row classes
+		rows.removeClass('alt first last');
+		var table = $(this).parents('table.sortable');
+		table.find('tr:even').addClass('alt');
+		table.find('tr:first').addClass('first');
+		table.find('tr:last').addClass('last');
+	});
+
+	/*---------------------------------
+		CSS Helpers
+	-----------------------------------*/
+	$('input[type=checkbox]').addClass('checkbox');
+	$('input[type=radio]').addClass('radio');
+	$('input[type=file]').addClass('file');
+	$('[disabled=disabled]').addClass('disabled');
+	$('table').find('tr:even').addClass('alt');
+	$('table').find('tr:first-child').addClass('first');
+	$('table').find('tr:last-child').addClass('last');
+	$('ul').find('li:first-child').addClass('first');
+	$('ul').find('li:last-child').addClass('last');
+	$('hr').before('<div class="clear">&nbsp;</div>');
+	$('[class*="col_"]').addClass('column');
+	$('pre').addClass('prettyprint');prettyPrint();
+
+});
+
+ 
+(function($){$.fn.tipTip=function(options){var defaults={activation:"hover",keepAlive:false,maxWidth:"200px",edgeOffset:3,defaultPosition:"bottom",delay:400,fadeIn:200,fadeOut:200,attribute:"title",content:false,enter:function(){},exit:function(){}};var opts=$.extend(defaults,options);if($("#tiptip_holder").length<=0){var tiptip_holder=$('<div id="tiptip_holder" style="max-width:'+opts.maxWidth+';"></div>');var tiptip_content=$('<div id="tiptip_content"></div>');var tiptip_arrow=$('<div id="tiptip_arrow"></div>');$("body").append(tiptip_holder.html(tiptip_content).prepend(tiptip_arrow.html('<div id="tiptip_arrow_inner"></div>')))}else{var tiptip_holder=$("#tiptip_holder");var tiptip_content=$("#tiptip_content");var tiptip_arrow=$("#tiptip_arrow")}return this.each(function(){var org_elem=$(this);if(opts.content){var org_title=opts.content}else{var org_title=org_elem.attr(opts.attribute)}if(org_title!=""){if(!opts.content){org_elem.removeAttr(opts.attribute)}var timeout=false;if(opts.activation=="hover"){org_elem.hover(function(){active_tiptip()},function(){if(!opts.keepAlive){deactive_tiptip()}});if(opts.keepAlive){tiptip_holder.hover(function(){},function(){deactive_tiptip()})}}else if(opts.activation=="focus"){org_elem.focus(function(){active_tiptip()}).blur(function(){deactive_tiptip()})}else if(opts.activation=="click"){org_elem.click(function(){active_tiptip();return false}).hover(function(){},function(){if(!opts.keepAlive){deactive_tiptip()}});if(opts.keepAlive){tiptip_holder.hover(function(){},function(){deactive_tiptip()})}}function active_tiptip(){opts.enter.call(this);tiptip_content.html(org_title);tiptip_holder.hide().removeAttr("class").css("margin","0");tiptip_arrow.removeAttr("style");var top=parseInt(org_elem.offset()['top']);var left=parseInt(org_elem.offset()['left']);var org_width=parseInt(org_elem.outerWidth());var org_height=parseInt(org_elem.outerHeight());var tip_w=tiptip_holder.outerWidth();var tip_h=tiptip_holder.outerHeight();var w_compare=Math.round((org_width-tip_w)/2);var h_compare=Math.round((org_height-tip_h)/2);var marg_left=Math.round(left+w_compare);var marg_top=Math.round(top+org_height+opts.edgeOffset);var t_class="";var arrow_top="";var arrow_left=Math.round(tip_w-12)/2;if(opts.defaultPosition=="bottom"){t_class="_bottom"}else if(opts.defaultPosition=="top"){t_class="_top"}else if(opts.defaultPosition=="left"){t_class="_left"}else if(opts.defaultPosition=="right"){t_class="_right"}var right_compare=(w_compare+left)<parseInt($(window).scrollLeft());var left_compare=(tip_w+left)>parseInt($(window).width());if((right_compare&&w_compare<0)||(t_class=="_right"&&!left_compare)||(t_class=="_left"&&left<(tip_w+opts.edgeOffset+5))){t_class="_right";arrow_top=Math.round(tip_h-13)/2;arrow_left=-12;marg_left=Math.round(left+org_width+opts.edgeOffset);marg_top=Math.round(top+h_compare)}else if((left_compare&&w_compare<0)||(t_class=="_left"&&!right_compare)){t_class="_left";arrow_top=Math.round(tip_h-13)/2;arrow_left=Math.round(tip_w);marg_left=Math.round(left-(tip_w+opts.edgeOffset+5));marg_top=Math.round(top+h_compare)}var top_compare=(top+org_height+opts.edgeOffset+tip_h+8)>parseInt($(window).height()+$(window).scrollTop());var bottom_compare=((top+org_height)-(opts.edgeOffset+tip_h+8))<0;if(top_compare||(t_class=="_bottom"&&top_compare)||(t_class=="_top"&&!bottom_compare)){if(t_class=="_top"||t_class=="_bottom"){t_class="_top"}else{t_class=t_class+"_top"}arrow_top=tip_h;marg_top=Math.round(top-(tip_h+5+opts.edgeOffset))}else if(bottom_compare|(t_class=="_top"&&bottom_compare)||(t_class=="_bottom"&&!top_compare)){if(t_class=="_top"||t_class=="_bottom"){t_class="_bottom"}else{t_class=t_class+"_bottom"}arrow_top=-12;marg_top=Math.round(top+org_height+opts.edgeOffset)}if(t_class=="_right_top"||t_class=="_left_top"){marg_top=marg_top+5}else if(t_class=="_right_bottom"||t_class=="_left_bottom"){marg_top=marg_top-5}if(t_class=="_left_top"||t_class=="_left_bottom"){marg_left=marg_left+5}tiptip_arrow.css({"margin-left":arrow_left+"px","margin-top":arrow_top+"px"});tiptip_holder.css({"margin-left":marg_left+"px","margin-top":marg_top+"px"}).attr("class","tip"+t_class);if(timeout){clearTimeout(timeout)}timeout=setTimeout(function(){tiptip_holder.stop(true,true).fadeIn(opts.fadeIn)},opts.delay)}function deactive_tiptip(){opts.exit.call(this);if(timeout){clearTimeout(timeout)}tiptip_holder.fadeOut(opts.fadeOut)}}})}})(jQuery);
+
+
+
+(function( $ ){
+
+  "use strict";
+
+  $.fn.fitVids = function( options ) {
+    var settings = {
+      customSelector: null
+    };
+
+    var div = document.createElement('div'),
+        ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
+
+    div.className = 'fit-vids-style';
+    div.innerHTML = '&shy;<style>         \
+      .fluid-width-video-wrapper {        \
+         width: 100%;                     \
+         position: relative;              \
+         padding: 0;                      \
+      }                                   \
+                                          \
+      .fluid-width-video-wrapper iframe,  \
+      .fluid-width-video-wrapper object,  \
+      .fluid-width-video-wrapper embed {  \
+         position: absolute;              \
+         top: 0;                          \
+         left: 0;                         \
+         width: 100%;                     \
+         height: 100%;                    \
+      }                                   \
+    </style>';
+
+    ref.parentNode.insertBefore(div,ref);
+
+    if ( options ) {
+      $.extend( settings, options );
+    }
+
+    return this.each(function(){
+      var selectors = [
+        "iframe[src*='player.vimeo.com']",
+        "iframe[src*='www.youtube.com']",
+        "iframe[src*='www.kickstarter.com']",
+        "object",
+        "embed"
+      ];
+
+      if (settings.customSelector) {
+        selectors.push(settings.customSelector);
+      }
+
+      var $allVideos = $(this).find(selectors.join(','));
+
+      $allVideos.each(function(){
+        var $this = $(this);
+        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
+            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
+            aspectRatio = height / width;
+        if(!$this.attr('id')){
+          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+          $this.attr('id', videoID);
+        }
+        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+        $this.removeAttr('height').removeAttr('width');
+      });
+    });
+  };
+})( jQuery );
+
+
+
+
